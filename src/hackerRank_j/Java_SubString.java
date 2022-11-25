4627a0793947e6e2233231b412480af53f38bf8d
GrandMostach0 <Kreedlegend0@gmail.com>
2022-11-25 15:32:11 -0600

    Java Substring completado

--- src/hackerRank_j/Java_SubString.java
+++ src/hackerRank_j/Java_SubString.java
@@ -1,4 +1,15 @@
 package hackerRank_j;
 
+import java.util.Scanner;
+
 public class Java_SubString {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int start = scan.nextInt();
+        int end = scan.nextInt();
+
+        System.out.println(s.substring(start, end));
+
+    }
 }
